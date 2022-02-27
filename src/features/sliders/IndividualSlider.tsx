a04c05254695ba7b2d487bd300cbfe5599f476a4
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2022-02-27 17:01:22 +0000

    MUI boilerplate

---
+++ src/features/sliders/IndividualSlider.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface IndividualSliderProps {
+    label: string;
+    sliderName: string;
+    value: boolean;
+    color: string;
+}
+
+export const IndividualSlider = ({
+
+}: IndividualSliderProps) => {
+
+}
